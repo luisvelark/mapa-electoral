8ca68a2a4400c9e239f4d500eb5db0f31db06171
Luis Velasquez Enciso <38818801+luisvelark@users.noreply.github.com>
2026-04-15 04:32:10 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
+export function middleware(req: NextRequest) {
+  const maintenance = true; // 🔥 cambia a false cuando termines
+
+  // Permitir APIs si quieres (opcional)
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    return NextResponse.next();
+  }
+
+  if (maintenance) {
+    return NextResponse.rewrite(new URL("/maintenance", req.url));
+  }
+
+  return NextResponse.next();
+}
